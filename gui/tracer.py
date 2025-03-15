fdb87ffc1ea711bff1f0553791de1602e403d658
Svetikovvv <151218780+Svetikovvv@users.noreply.github.com>
2025-03-15 13:27:18 +0300

    пупупу2

--- gui/tracer.py
+++ gui/tracer.py
@@ -2,16 +2,16 @@ from collections import deque
 
 class Tracer:
     """
-    Класс для двунаправленной трассировки (bidirectional wave),
-    где при появлении пересечений в одной итерации выбирается то,
-    у которого минимальная сумма приоритетов (wave_start[r][c] + wave_finish[r][c]).
+    Двунаправленная трассировка методом встречной волны.
+    Вместо iteration*10 + priority каждая новая ячейка получает уникальный порядковый номер
+    в порядке, в котором она извлекается из очереди BFS (соблюдая приоритет просмотра соседей).
     """
 
     def __init__(self, grid):
         """
         :param grid: 2D-список (копия поля), где:
                      0 – свободная клетка, 1 – препятствие,
-                     2(A) и 3(B) здесь уже заменены на 0, чтобы волна могла идти.
+                     2(A) и 3(B) уже заменены на 0, чтобы волна могла идти.
         """
         self.grid = grid
         self.rows = len(grid)
@@ -19,80 +19,89 @@ class Tracer:
 
     def bidirectional_trace(self, start, finish):
         """
-        Выполняет двунаправленный BFS. На каждом уровне:
-          1) Расширяет волны старта
-          2) Расширяет волны финиша
-          3) Ищет новые пересечения
-             Если они есть, выбирает клетку с минимальной суммой composite_value
-             (wave_start[r][c] + wave_finish[r][c]) и останавливается.
+        Запускает двунаправленную волну: BFS от A и BFS от B.
+        На каждом "уровне" мы берём все клетки очереди старта и все клетки очереди финиша,
+        расширяем их соседей в порядке (вверх, вправо, вниз, влево).
+        Каждая новая клетка получает уникальный порядковый номер (label_s или label_f).
+        Если при расширении появляются пересечения, выбираем клетку с минимальной суммой (label_s + label_f).
 
-        :param start: (r, c) координаты A
-        :param finish: (r, c) координаты B
-        :return: (wave_start, wave_finish, best_meeting)
-                 wave_start[r][c] = (cv, dname)
-                 wave_finish[r][c] = (cv, dname)
-                 best_meeting – клетка пересечения или None
+        :param start: (sr, sc) координаты A
+        :param finish: (fr, fc) координаты B
+        :return: (wave_start, wave_finish, meeting_point)
+                 wave_start[r][c] = (label_s, direction_s),
+                 wave_finish[r][c] = (label_f, direction_f),
+                 meeting_point – клетка пересечения или None
         """
-        # Приоритет направлений
-        direction_priority = {"U": 1, "R": 2, "D": 3, "L": 4}
-
-        # Матрицы для (composite_value, direction)
+        # Матрицы для хранения (label, direction)
         wave_start = [[None for _ in range(self.cols)] for _ in range(self.rows)]
         wave_finish = [[None for _ in range(self.cols)] for _ in range(self.rows)]
 
+        # Очереди BFS
         q_start = deque()
         q_finish = deque()
 
-        s_r, s_c = start
-        f_r, f_c = finish
-        wave_start[s_r][s_c] = (0, None)
-        wave_finish[f_r][f_c] = (0, None)
-        q_start.append(start)
-        q_finish.append(finish)
+        sr, sc = start
+        fr, fc = finish
 
-        iteration = 0
+        # Стартовые точки получают номер 0
+        wave_start[sr][sc] = (0, None)
+        wave_finish[fr][fc] = (0, None)
+        q_start.append((sr, sc))
+        q_finish.append((fr, fc))
 
-        def is_valid(r, c, visited):
-            return (0 <= r < self.rows and 0 <= c < self.cols
-                    and self.grid[r][c] == 0
-                    and visited[r][c] is None)
+        # Счётчики для новой нумерации
+        # Каждая новая клетка, открытая волной старта, получает label_s + 1
+        # Каждая новая клетка, открытая волной финиша, получает label_f + 1
+        label_counter_s = 1
+        label_counter_f = 1
 
-        # Можно упорядочить направления в любом порядке (U, R, D, L) и т.д.
+        # Порядок просмотра соседей: верх, вправо, вниз, влево
         directions = [
             ("U", -1, 0),
             ("R", 0, 1),
             ("D", 1, 0),
-            ("L", 0, -1)
+            ("L", 0, -1),
         ]
 
-        while q_start and q_finish:
-            iteration += 1
+        def is_valid(r, c, wave):
+            return (0 <= r < self.rows and 0 <= c < self.cols
+                    and self.grid[r][c] == 0
+                    and wave[r][c] is None)
 
-            # 1) Расширяем волну старта
+        best_meeting = None
+        best_sum = None
+
+        # Пока есть клетки в обеих очередях
+        while q_start and q_finish:
+            # 1) Расширяем волну от старта (A)
             new_start_cells = []
             for _ in range(len(q_start)):
                 r, c = q_start.popleft()
+                current_label_s, _ = wave_start[r][c]
+                # Если мы уже дошли до B, можно проверять пересечение
                 for dname, dr, dc in directions:
                     nr, nc = r + dr, c + dc
                     if is_valid(nr, nc, wave_start):
-                        cv = iteration * 10 + direction_priority[dname]
-                        wave_start[nr][nc] = (cv, dname)
+                        # Присваиваем следующий номер
+                        wave_start[nr][nc] = (label_counter_s, dname)
+                        label_counter_s += 1
                         q_start.append((nr, nc))
                         new_start_cells.append((nr, nc))
 
-            # 2) Расширяем волну финиша
+            # 2) Расширяем волну от финиша (B)
             new_finish_cells = []
             for _ in range(len(q_finish)):
                 r, c = q_finish.popleft()
+                current_label_f, _ = wave_finish[r][c]
                 for dname, dr, dc in directions:
                     nr, nc = r + dr, c + dc
                     if is_valid(nr, nc, wave_finish):
-                        cv = iteration * 10 + direction_priority[dname]
-                        wave_finish[nr][nc] = (cv, dname)
+                        wave_finish[nr][nc] = (label_counter_f, dname)
+                        label_counter_f += 1
                         q_finish.append((nr, nc))
                         new_finish_cells.append((nr, nc))
 
-            # 3) Проверяем новые пересечения
+            # 3) Проверяем пересечения среди новых клеток
             intersections = []
             for (r, c) in new_start_cells:
                 if wave_finish[r][c] is not None:
@@ -102,58 +111,60 @@ class Tracer:
                     intersections.append((r, c))
 
             if intersections:
-                # Выбираем среди них клетку с минимальной суммой
-                best = None
-                best_sum = None
+                # Ищем минимальную сумму (label_s + label_f)
                 for (r, c) in intersections:
-                    cv_s, _ = wave_start[r][c]
-                    cv_f, _ = wave_finish[r][c]
-                    s = cv_s + cv_f
+                    label_s_val, _ = wave_start[r][c]
+                    label_f_val, _ = wave_finish[r][c]
+                    s = label_s_val + label_f_val
                     if best_sum is None or s < best_sum:
                         best_sum = s
-                        best = (r, c)
+                        best_meeting = (r, c)
+                return wave_start, wave_finish, best_meeting
 
-                return wave_start, wave_finish, best
-
-        # Если не нашли пересечений и очереди исчерпаны
+        # Если очереди опустели, пересечений нет
         return wave_start, wave_finish, None
 
     def step_by_step_trace(self, start, finish):
         """
-        Генератор: на каждом "шаге" (итерации) расширяем волны старта и финиша,
-        затем проверяем новые пересечения. Если пересечения есть, выбираем минимальное
-        и завершаем. Если нет – продолжаем.
-        """
-        from collections import deque
-        direction_priority = {"U": 1, "R": 2, "D": 3, "L": 4}
+        Генератор для пошаговой (по уровням) двунаправленной волны.
+        На каждом "уровне" извлекаем все клетки очереди старта, затем все клетки очереди финиша,
+        назначаем новые номера в порядке обнаружения. Если есть пересечения – завершаем.
 
+        yield (iteration, wave_start, wave_finish, best_meeting)
+        """
         wave_start = [[None for _ in range(self.cols)] for _ in range(self.rows)]
         wave_finish = [[None for _ in range(self.cols)] for _ in range(self.rows)]
         q_start = deque()
         q_finish = deque()
 
-        s_r, s_c = start
-        f_r, f_c = finish
-        wave_start[s_r][s_c] = (0, None)
-        wave_finish[f_r][f_c] = (0, None)
-        q_start.append(start)
-        q_finish.append(finish)
+        sr, sc = start
+        fr, fc = finish
 
-        iteration = 0
+        wave_start[sr][sc] = (0, None)
+        wave_finish[fr][fc] = (0, None)
+        q_start.append((sr, sc))
+        q_finish.append((fr, fc))
 
-        def is_valid(r, c, visited):
-            return (0 <= r < self.rows and 0 <= c < self.cols
-                    and self.grid[r][c] == 0
-                    and visited[r][c] is None)
+        # Счётчики для новой нумерации
+        label_counter_s = 1
+        label_counter_f = 1
 
         directions = [
             ("U", -1, 0),
             ("R", 0, 1),
             ("D", 1, 0),
-            ("L", 0, -1)
+            ("L", 0, -1),
         ]
 
-        # Пока есть клетки для расширения
+        def is_valid(r, c, wave):
+            return (0 <= r < self.rows and 0 <= c < self.cols
+                    and self.grid[r][c] == 0
+                    and wave[r][c] is None)
+
+        iteration = 0
+        best_meeting = None
+        best_sum = None
+
         while q_start or q_finish:
             iteration += 1
 
@@ -163,8 +174,8 @@ class Tracer:
                 for dname, dr, dc in directions:
                     nr, nc = r + dr, c + dc
                     if is_valid(nr, nc, wave_start):
-                        cv = iteration * 10 + direction_priority[dname]
-                        wave_start[nr][nc] = (cv, dname)
+                        wave_start[nr][nc] = (label_counter_s, dname)
+                        label_counter_s += 1
                         q_start.append((nr, nc))
                         new_start_cells.append((nr, nc))
 
@@ -174,8 +185,8 @@ class Tracer:
                 for dname, dr, dc in directions:
                     nr, nc = r + dr, c + dc
                     if is_valid(nr, nc, wave_finish):
-                        cv = iteration * 10 + direction_priority[dname]
-                        wave_finish[nr][nc] = (cv, dname)
+                        wave_finish[nr][nc] = (label_counter_f, dname)
+                        label_counter_f += 1
                         q_finish.append((nr, nc))
                         new_finish_cells.append((nr, nc))
 
@@ -188,34 +199,27 @@ class Tracer:
                 if wave_start[r][c] is not None:
                     intersections.append((r, c))
 
-            # Возвращаем текущее состояние
             if intersections:
-                # Выбираем лучшее пересечение
-                best = None
-                best_sum = None
                 for (r, c) in intersections:
-                    cv_s, _ = wave_start[r][c]
-                    cv_f, _ = wave_finish[r][c]
-                    s = cv_s + cv_f
-                    if best_sum is None or s < best_sum:
-                        best_sum = s
-                        best = (r, c)
+                    ls, _ = wave_start[r][c]
+                    lf, _ = wave_finish[r][c]
+                    total = ls + lf
+                    if best_sum is None or total < best_sum:
+                        best_sum = total
+                        best_meeting = (r, c)
+                yield iteration, wave_start, wave_finish, best_meeting
+                return
 
-                yield iteration, wave_start, wave_finish, best
-                return  # Останавливаемся
-
-            # Если нет пересечений, просто возвращаем состояние
             yield iteration, wave_start, wave_finish, None
 
-        # Если очереди исчерпаны, путь не найден
+        # Нет пути
         yield iteration, wave_start, wave_finish, None
-
 
     @staticmethod
     def reconstruct_path(wave, origin, meeting):
         """
-        Восстанавливает путь в матрице wave от origin до meeting.
-        wave[r][c] = (composite_value, direction).
+        Восстанавливает путь в матрице wave (label, direction),
+        двигаясь "назад" по direction: U->(r+1), R->(c-1), D->(r-1), L->(c+1).
         """
         path = []
         r, c = meeting
